e938ff2d5004192a55edffa780b68cffa3bca34c
Gerben <gerbenvaneerten@gmail.com>
2017-09-03 20:15:24 +0200

    Initial version

---
+++ src/ButtonThing.ino
@@ -0,0 +1,81 @@
+#include <Thing.h>
+#include <BlinkPattern.h>
+
+#define BUTTON_PIN D3
+
+using namespace g3rb3n;
+
+Thing thing;
+BlinkPattern led(BUILTIN_LED);
+
+BlinkPattern::Pattern<2> initialize{{1,1},50};
+BlinkPattern::Pattern<2> on{{39,1},25};
+BlinkPattern::Pattern<2> off{{1,39},25};
+BlinkPattern::Pattern<2> reqOn{{1,9},25};
+BlinkPattern::Pattern<2> reqOff{{9,1},25};
+
+bool state = true;
+bool buttonState = false;
+bool feedbackState = false;
+
+
+void setup()
+{
+  Serial.begin(230400);
+  Serial.println();
+
+  Serial.println("ClientID:" + thing.clientId());
+
+  led.begin();
+
+  pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  led.setPattern(initialize);
+  thing.onStateChange([](const String& msg){
+    Serial.println(msg);
+  });
+  String sensor = "sensor/button/";
+  sensor += thing.clientId();
+  thing.addSensor(sensor, 1000, [](Value& value){
+    value = state;
+  });
+
+  String display = "display/button/";
+  display += thing.clientId();
+  thing.addActuator(display, [](Value& value){
+    Serial.println("Got " + (String)value);
+    feedbackState = (bool)value;
+    Serial.println("Got " + feedbackState);
+    //digitalWrite(BUILTIN_LED, !feedbackState);
+    
+    if(feedbackState)
+      led.setPattern(on);
+    else
+      led.setPattern(off);
+  });
+  
+  thing.begin();
+}
+
+void loop()
+{
+  handle();
+  led.handle();
+  thing.handle();
+}
+
+void handle()
+{
+  bool pushed = !digitalRead(BUTTON_PIN);
+  if (pushed && pushed != buttonState)
+  {
+    state = !state;
+    if(state)
+      led.setPattern(reqOn);
+    else
+      led.setPattern(reqOff);
+    
+  }
+  buttonState = pushed;
+}
